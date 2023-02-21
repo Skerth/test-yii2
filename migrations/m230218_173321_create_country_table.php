e58a4422ef84e7847a4f76ce7677d301ec4a7067
Сергей <sklyatov@gmail.com>
2023-02-21 09:01:14 +0300

    Правка миграции

--- migrations/m230218_173321_create_country_table.php
+++ migrations/m230218_173321_create_country_table.php
@@ -12,11 +12,15 @@ class m230218_173321_create_country_table extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('{{%country}}', [
-            'code' => 'char(2) NOT NULL PRIMARY KEY',
-            'name' => $this->string(50)->notNull(),
-            'population' =>  $this->integer(11)->notNull()->defaultValue(0),
-        ]);
+        $table_isset = Yii::$app->db->getTableSchema('country');
+
+        if(!isset($table_isset)) {
+            $this->createTable('{{%country}}', [
+                'code' => 'char(2) NOT NULL PRIMARY KEY',
+                'name' => $this->string(50)->notNull(),
+                'population' =>  $this->integer(11)->notNull()->defaultValue(0),
+            ]);
+        }
     }
 
     /**
