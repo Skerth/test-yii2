c3e9e09ea04e04e9b0dab88a7c00d8db7070b9b2
Сергей <sklyatov@gmail.com>
2023-03-09 15:21:19 +0300

    Сборка массива задач для письма

--- commands/ReminderController.php
+++ commands/ReminderController.php
@@ -16,6 +16,7 @@ class ReminderController extends Controller
 {
     public function actionIndex()
     {
+        $arrTasks = []; // Задачи, которые нужно отправить в письме.
         $checkDate = date('Y-m-d', strtotime('+5day'));
 
         $tasks = ClientsTask::find()
@@ -31,23 +32,35 @@ class ReminderController extends Controller
             $modelTask = ClientsTask::findOne($task->id);
             $modelTask->archive = 1;
             $modelTask->update();
+
+            $arrTasks[] = [
+                'id' => $task->id,
+                'client' => $task->client->name,
+                'service' => ClientsTask::servicesArr[$task->service],
+                'check_date' => $task->check_date,
+                'price' => $task->price,
+                'note' => $task->note,
+            ];
         }
 
-        $from = "noreply@do-sites.ru";
-        $to = "sklyatov@gmail.com";
-        $subject = "Checking PHP mail";
-        $message = "PHP mail works just fine";
-        $headers = [
-            'From' => "Do Sites CRM <$from>",
-            'X-Sender' => "Do Sites CRM <$from>",
-            'X-Mailer' => 'PHP/' . phpversion(),
-            'X-Priority' => '1',
-            'Return-Path' => "$from",
-            'MIME-Version' => '1.0',
-            'Content-Type' => 'text/html; charset=iso-8859-1'
-        ];
-        mail($to,$subject,$message, $headers);
-        echo "The email message was sent.";
+        if (count($arrTasks))
+        {
+            $from = "noreply@do-sites.ru";
+            $to = "sklyatov@gmail.com";
+            $subject = "Checking PHP mail";
+            $message = "PHP mail works just fine";
+            $headers = [
+                'From' => "Do Sites CRM <$from>",
+                'X-Sender' => "Do Sites CRM <$from>",
+                'X-Mailer' => 'PHP/' . phpversion(),
+                'X-Priority' => '1',
+                'Return-Path' => "$from",
+                'MIME-Version' => '1.0',
+                'Content-Type' => 'text/html; charset=iso-8859-1'
+            ];
+            mail($to,$subject,$message, $headers);
+            echo "The email message was sent.";
+        }
 
         return ExitCode::OK;
     }
